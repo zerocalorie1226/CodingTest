74ce4cc4994319528033a316ba8abe7058007fa0
KNom <65861663+zerocalorie1226@users.noreply.github.com>
2025-04-21 17:01:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer=45
+    num=0
+    for i in range(0,10):
+        if (i in numbers):
+            num+=i
+                
+    
+    
+    
+    return answer-num
